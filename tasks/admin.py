68fd39fa425703d47d92fe7ef24688525bef2528
hellodev101 <soraya.preechadech@gmail.com>
2024-08-17 19:59:25 +0200

    TaskAdmin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from tasks.models import Task
 
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "status", "owner", "created_at",
+                    "updated_at")
+    list_filter = ("status",)
+
+admin.site.register(Task, TaskAdmin)
