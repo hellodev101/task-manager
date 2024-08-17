073b060fb17cabf4993c289409b9606ef01186fb
hellodev101 <soraya.preechadech@gmail.com>
2024-08-17 19:37:52 +0200

    index url & tasks namespace

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
 
+app_name = "tasks"
+
+urlpatterns = [   
+    path("", views.index, name="index"),   
+]
