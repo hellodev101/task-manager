5fb361e241d96445619f41c705bcbf99c09ae196
hellodev101 <soraya.preechadech@gmail.com>
2024-08-23 13:56:27 +0200

    added STATICFILE_DIR

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -127,7 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+   (os.path.join(BASE_DIR, "tasks/static"))
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 
 # Default primary key field type
